bd5bf8939274a38068a0859cb36ff85f8df401b9
Deepakpd811 <deepak0413.be21@chitkara.edu.in>
2025-04-05 13:24:59 +0530

    [Deepak]add: read json object using jakson program of json filehandling file added

---
+++ src/main/java/com/bridgelab/json_filehandling/practice_problem/read_json/Main.java
@@ -0,0 +1,11 @@
+package com.bridgelab.json_filehandling.practice_problem.read_json;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String filepath = "files/student.json";
+
+        ReadJson.readJson(filepath);
+
+    }
+}
